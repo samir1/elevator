7bfed09730a7cc239784afcb3bb6e034d792ddc4
Samir <samir1@brandeis.edu>
2015-04-30 19:57:04 -0400

    change building

--- sim.rb
+++ sim.rb
@@ -6,14 +6,14 @@ load './simulation.rb'
 
 
 person = Person.new 0, 2
-person2 = Person.new 0, 1
-building = Building.new 3, 1
+person2 = Person.new 0, 4
+building = Building.new 5, 1
 sim = Simulation.new
 sim.register building
 sim.register person
 sim.register person2
 building.add_person person
 building.add_person person2
-sim.run 4
+sim.run 6
 
 # creates a building and adds two people to it
